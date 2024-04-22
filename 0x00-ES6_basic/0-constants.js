1eebf87e6e9746af64ff70d7b30f4d52fcc4adce
Natnael12119 <nhattydeveloper@gmail.com>
2024-04-22 22:44:19 +0000

    Debugging

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -3,10 +3,6 @@ export function taskFirst() {
   return task;
 }
 
-export function getLast() {
-  return ' is okay';
-}
-
 export function taskNext() {
   let combination = 'But sometimes let';
   combination += getLast();
