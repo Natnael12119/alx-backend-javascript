d80f15a89856a76b085dc19f9b66aa93cfe4f2ea
Natnael12119 <nhattydeveloper@gmail.com>
2024-04-25 10:43:51 +0000

    Debugging

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,12 +1,4 @@
 export default class ClassRoom{
 	constructor(maxStudentsSize){
-		if (tyeof maxStudentsSize !== 'nimber' || maxStudentsSize <= 0) {
-			trow new Error('maxStudentsSize must be a positive number');
-		}
-
-		this._maxStudentsSize = maxStudentsSize;
-	}
-
-	get maxStudentsSize(){
-		return this._maxStudentSize;
+		this._maxStudentSize;
 	}
