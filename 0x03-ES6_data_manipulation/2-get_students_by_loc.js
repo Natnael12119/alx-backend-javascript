2791e2eef4b06cbffb488e1b4affec768eb9b734
Natnael12119 <nhattydeveloper@gmail.com>
2024-05-03 20:52:44 +0000

    Debugging

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,4 +1,5 @@
 export default function getStudentsByLocation(students, city) {
+  // eslint-disable-next-line array-callback-return, consistent-return
   const result = students.filter((obj) => {
     if (obj.location === city) {
       return obj;
